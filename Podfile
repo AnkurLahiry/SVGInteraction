c882988b7214b830fbce6dc1843ccd25ade40e4f
Ankur <ankur@ridmik.net>
2020-08-15 12:13:56 +0600

    Code Refactored.

--- Podfile
+++ Podfile
@@ -5,7 +5,7 @@ target 'SVGInteraction' do
   # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  pod 'Macaw'
+  pod 'Macaw', '0.9.5'
 
   # Pods for SVGInteraction
 
--- SVGInteraction/AppDelegate.swift
+++ SVGInteraction/AppDelegate.swift
@@ -16,20 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        let screen = UIScreen.main.bounds
-        
-        window = UIWindow(frame: screen)
-        
-        let navViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "navViewController") as! UINavigationController
-        window?.rootViewController = navViewController
-        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    
-
 
 }
 
